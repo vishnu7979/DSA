dd3ec838bd9aa1e9f857e7e7ad375ef39f849e86
vishnu <vishnushaji797@gmail.com>
2023-12-16 10:47:00 +0530

    16122023

---
+++ week3/trees/binarysearchtree.js
@@ -0,0 +1,20 @@
+class node{
+    constructor(value){
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class BinarySearchTree{
+   constructor(){
+    this.root=null;
+   }
+
+   isEmpty(){
+    return this.root===null;
+   }
+
+   
+
+}
