f777a5b2dbb17c80f4531ee42017160129f2b64c
vishnu <vishnushaji797@gmail.com>
2024-02-05 00:09:13 +0530

    SDFsdaf

--- week3/graphs/realGraph.js
+++ week3/graphs/realGraph.js
@@ -106,7 +106,7 @@ class Graph{
     const graph=new Graph();
     graph.addVertex('A');
     graph.addVertex('B');
-    graph.addVertex('C'); 
+    graph.addVertex('C');
     graph.addEdge('A','B')
     graph.addEdge('B','C');
     console.log(graph.hasEdge('C','B'));
