757be319269dcc11b7f6e6f9fdb5acebc46b01c3
vishnu <vishnushaji797@gmail.com>
2024-02-08 00:43:45 +0530

    werwe

--- week3/trees/realtree.js
+++ week3/trees/realtree.js
@@ -111,7 +111,7 @@ class BinarySearchTree{
 
     deleteNode(root,value){
         if(root===null){
-            return root
+            return root 
         }
         if(value<root.value){
             root.left=this.deleteNode(root.left,value)
